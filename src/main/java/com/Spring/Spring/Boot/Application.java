482dfaf52a8dce37dd83611d9a76c7ec6cdef3e9
Avirup De <avirupde28@gmail.com>
2022-05-09 19:36:36 +0530

    first commit

---
+++ src/main/java/com/Spring/Spring/Boot/Application.java
@@ -0,0 +1,14 @@
+package com.Spring.Spring.Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		System.out.print("Spring Boot application by Avirup De");
+	}
+
+}
